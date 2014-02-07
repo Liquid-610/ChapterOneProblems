3e0cbe2f0789f14afe5fd06900b105bfa284eb4e
Liquid-610 <jamesbarnes71@gmail.com>
2014-02-07 10:12:43 -0500

    Initials

---
+++ ConsoleProblemsOne/Initials/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("      J  BBBBBBB");
+            System.Console.WriteLine("      J  B     B");
+            System.Console.WriteLine("      J  BBBBBBB");  
+            System.Console.WriteLine("      J  BBBB");     
+            System.Console.WriteLine("      J  BBBBBBB");      
+            System.Console.WriteLine("J     J  B     B");       
+            System.Console.WriteLine("J     J  B     B");   
+            System.Console.WriteLine("J     J  BBBBBBB");  
+            System.Console.WriteLine("JJJJJJJ");        
+            
+        }
+    }
+}
