fb7e9dcbc38722045232eb498d0f6c3ce494a8cc
Liquid-610 <jamesbarnes71@gmail.com>
2014-02-07 09:55:17 -0500

    Tree is done

---
+++ ConsoleProblemsOne/Tree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("    x   ");
+            System.Console.WriteLine("   xxx  ");
+            System.Console.WriteLine("  xxxxx ");
+            System.Console.WriteLine(" xxxxxxx");
+            System.Console.WriteLine("    x   ");
+        }
+    }
+}
